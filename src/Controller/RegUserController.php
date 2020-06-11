3e312ef03c96393ada52a1ff7685d77374887c29
Saief Zaneti <saief_zaneti@yahoo.com>
2020-06-11 23:14:43 +0100

    Addded basic templates for profile/settings

---
+++ src/Controller/RegUserController.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\Routing\Annotation\Route;
+
+class RegUserController extends AbstractController
+{
+    /**
+     * @Route("/reguser/profile", name="reguser_profile")
+     */
+    public function profile()
+    {
+        return $this->render('reguser/profile.html.twig', [
+        ]);
+    }
+
+    /**
+     * @Route("/reguser/settings",name="reguser_settings")
+     */
+    public function settings()
+    {
+        $this->denyAccessUnlessGranted('IS_AUTHENTICATED_FULLY');
+        return $this->render('reguser/settings.html.twig', [
+        ]);
+
+    }
+}
