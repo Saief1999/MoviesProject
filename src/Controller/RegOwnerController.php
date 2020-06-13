0cfc8153cd85c111bab4eb17df9de4a7c71038a9
aminfelah <60293387+aminfelah@users.noreply.github.com>
2020-06-13 03:17:09 +0200

    added the owner profile space --- still workin on access denied because of roles

---
+++ src/Controller/RegOwnerController.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\Routing\Annotation\Route;
+
+class RegOwnerController extends AbstractController
+{
+    /**
+     * @Route("/regowner/profile", name="regowner_profile")
+     */
+    public function profile()
+    {
+        return $this->render('reg_owner/profile.html.twig');
+    }
+    /**
+     * @Route("/regowner/settings",name="regowner_settings")
+     */
+    public function settings()
+    {
+        /* $this->denyAccessUnlessGranted('IS_AUTHENTICATED_FULLY');*/
+        return $this->render('reg_owner/settings.html.twig', [
+        ]);
+
+    }
+}
