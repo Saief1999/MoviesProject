86342718bd6782d74f4312992b026ecb9acf7e84
aminfelah <60293387+aminfelah@users.noreply.github.com>
2020-06-17 01:45:45 +0200

    added the user settings edit

--- src/Controller/RegUserController.php
+++ src/Controller/RegUserController.php
@@ -2,7 +2,13 @@
 
 namespace App\Controller;
 
+use App\Entity\CinemaOwner;
+use App\Entity\RegisteredUser;
+use App\Form\CinemaOwnerFormType;
+use App\Form\RegisteredUserFormType;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\File\Exception\FileException;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\Routing\Annotation\Route;
 
 class RegUserController extends AbstractController
@@ -19,10 +25,23 @@ class RegUserController extends AbstractController
     /**
      * @Route("/reguser/settings",name="reguser_settings")
      */
-    public function settings()
-    {
-        return $this->render('reguser/settings.html.twig', [
-        ]);
+    public function settings(Request $request,  RegisteredUser $registreduser=null) {
+        $session = $this->get('session');
+        $user = $this->getUser();
+        $repository =$this->getDoctrine()->getRepository(RegisteredUser::class);
+        $registreduser=$repository->findOneBy(array('user'=>$user));
 
+
+        $form = $this->createForm(RegisteredUserFormType::class, $registreduser);
+        $form->handleRequest($request);
+        if ($form->isSubmitted() && $form->isValid()) {
+            $entityManager = $this->getDoctrine()->getManager();
+            $entityManager->persist($registreduser);
+            $entityManager->flush();
+            return $this->redirectToRoute('home');
+        }
+        return $this->render('reguser/settings.html.twig', array(
+            'form' => $form->createView()
+        ));
     }
 }
