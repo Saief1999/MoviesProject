6ad21d52d3f3c65e8259342f9d09fb2bcd676fe0
Saief Zaneti <saief_zaneti@yahoo.com>
2020-06-16 13:25:08 +0100

    added custom exception page

---
+++ src/Security/AccessDeniedHandler.php
@@ -0,0 +1,17 @@
+<?php
+namespace App\Security;
+
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Security\Core\Exception\AccessDeniedException;
+use Symfony\Component\Security\Http\Authorization\AccessDeniedHandlerInterface;
+
+class AccessDeniedHandler implements AccessDeniedHandlerInterface
+{
+    public function handle(Request $request, AccessDeniedException $accessDeniedException)
+    {
+
+        return new Response("<h1>You don't have the Rights to access this page</h1>", 403);
+
+    }
+}
