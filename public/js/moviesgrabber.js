a57d6d22f6c392878b7134b583de5afa300b6b41
Saief Zaneti <saief_zaneti@yahoo.com>
2020-06-28 00:30:55 +0100

    fixed bug where movie page doesn't load

--- public/js/moviesgrabber.js
+++ public/js/moviesgrabber.js
@@ -44,8 +44,20 @@ function getMovie(){
   axios.get("https://api.themoviedb.org/3/movie/" + movieId + "?api_key=98325a9d3ed3ec225e41ccc4d360c817")
     .then(function (response) {
     let movie = response.data;
+    console.log(movie) ;
         let poster= (movie.poster_path!=null)?"https://image.tmdb.org/t/p/w500"+movie.poster_path:"/images/altImageMovie/notfoundLast.png" ;
-    let output = `
+
+        let genres = "" ;
+        for(i=0 ; i<movie.genres.length;i++)
+        {genres=genres+movie.genres[i].name +
+            ((i==movie.genres.length-1)? " .":" , "); }
+
+        let production_companies ="";
+        for(i=0 ; i<movie.production_companies.length;i++)
+        {production_companies=production_companies+movie.production_companies[i].name+" "
+            +((i==movie.production_companies.length-1)? " .":" , "); }
+
+        let output = `
         <div class="row">
           <div class="col-md-4">
             <img src="${poster}" class="thumbnail" alt="image">
@@ -53,11 +65,11 @@ function getMovie(){
           <div class="col-md-8">
             <h2>${movie.title}</h2>
             <ul class="list-group">
-              <li class="list-group-item"><strong>Genre:</strong> ${movie.genres[0].name},${movie.genres[1].name}</li>
+              <li class="list-group-item"><strong>Genre:</strong> ${genres}</li>
               <li class="list-group-item"><strong>Released:</strong> ${movie.release_date}</li>
               <li class="list-group-item"><strong>Rated:</strong> ${movie.vote_average}</li>
               <li class="list-group-item"><strong>Runtime:</strong> ${movie.runtime} min.</li>
-              <li class="list-group-item"><strong>Production Companies:</strong> ${movie.production_companies[0].name} min.</li>
+              <li class="list-group-item"><strong>Production Companies:</strong> ${production_companies}</li>
             </ul>
           </div>
         </div>
