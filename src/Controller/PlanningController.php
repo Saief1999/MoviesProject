f185dfd5ac0758a066a447ea99c493b8121d8c50
Saief Zaneti <saief_zaneti@yahoo.com>
2020-06-28 21:01:01 +0100

    Added delete functionality and 404 page

--- src/Controller/PlanningController.php
+++ src/Controller/PlanningController.php
@@ -2,23 +2,20 @@
 
 namespace App\Controller;
 
-use App\Entity\CustomPersonne;
 use App\Entity\Planning;
-use App\Form\CustomPersonneType;
 use App\Form\PlanningFormType;
 use Doctrine\ORM\EntityManagerInterface;
-use http\Env\Request;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
-use Symfony\Component\HttpFoundation\File\Exception\FileException;
 use Symfony\Component\Routing\Annotation\Route;
 
 class PlanningController extends AbstractController
 {
     /**
-     * @Route("/planning", name="planning")
+     * @Route("/regowner/plannings/{id}", name="planning")
      */
-    public function index()
+    public function planning(Planning $planning=null)
     {
+        if ($planning==null) return $this->render("404.html.twig") ;
         return $this->render('planning/index.html.twig', [
             'controller_name' => 'PlanningController',
         ]);
@@ -27,7 +24,6 @@ class PlanningController extends AbstractController
     /**
      * @Route("/regowner/plannings",name="myplannings")
      */
-
     public function plannings(\Symfony\Component\HttpFoundation\Request $request)
     {
         $pageActuelle = $request->query->get('page')??1;
@@ -63,5 +59,20 @@ class PlanningController extends AbstractController
         }
         else return $this->render("planning/add_planning.html.twig",["form"=>$form->createView()]) ;
 
+    }
+
+    /**
+     * @Route("/regowner/plannings/remove/{id}",name="remove_planning")
+     */
+    public function removePlanning(EntityManagerInterface $em ,Planning $planning=null)
+    {
+        if ($planning==null) return $this->render('404.html.twig') ;
+        else
+        {
+            $em ->remove($planning) ;
+            $em->flush();
+            $this->addFlash('success',"Planning deleted successfully");
+            return $this->redirectToRoute("myplannings",['p'=>1]);
+        }
     }
 }
