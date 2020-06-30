015263fd3380d9137b9ea358cb5cfd79af8283e3
Saief Zaneti <saief_zaneti@yahoo.com>
2020-06-30 21:35:48 +0100

    Added genres while saving

--- public/js/planning/planning-movie-grabber.js
+++ public/js/planning/planning-movie-grabber.js
@@ -15,7 +15,6 @@ $(document).ready(function(){
         let title=($(this).find(".movie_title").text()) ;
         $('#search').val(title);
        let link=$(this).find(".tmdb_link").text() ;
-       let imdb=$(this).find(".imdb_link").text() ;
         axios.get('https://api.themoviedb.org/3/movie/'+link+'?api_key=98325a9d3ed3ec225e41ccc4d360c817&language=en-US')
             .then(function (response) {
                 let  movie=response.data;
