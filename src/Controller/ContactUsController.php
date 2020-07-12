60f27fd7da13a494e6a1c2ee35dd42d5b18c96a4
Saief Zaneti <saief_zaneti@yahoo.com>
2020-07-12 16:32:54 +0100

    Finished contact Us page

--- src/Controller/ContactUsController.php
+++ src/Controller/ContactUsController.php
@@ -3,8 +3,13 @@
 namespace App\Controller;
 
 use App\Form\ContactUsFormType;
+use Symfony\Bridge\Twig\Mime\TemplatedEmail;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\Mailer\Exception\TransportExceptionInterface;
+use Symfony\Component\Mailer\MailerInterface;
+use Symfony\Component\Mime\Address;
+use Symfony\Component\Mime\Email;
 use Symfony\Component\Routing\Annotation\Route;
 
 class ContactUsController extends AbstractController
@@ -12,7 +17,7 @@ class ContactUsController extends AbstractController
     /**
      * @Route("/contactus", name="contact_us")
      */
-    public function index(Request $request)
+    public function index(Request $request , MailerInterface $mailer)
     {
 
         $form = $this->createForm(ContactUsFormType::class) ;
@@ -26,10 +31,25 @@ class ContactUsController extends AbstractController
         $form->handleRequest($request) ;
         if ($form->isSubmitted() && $form->isValid()  )
         {
+            $name = $form ->get('name')->getData() ;
+            $email = $form->get('email')->getData() ;
+            $message = $form->get('message')->getData() ;
+            $mail = (new TemplatedEmail())
+                ->from(new Address('zanetisaief.noreply@gmail.com', 'Email Sender Bot'))
+                ->to('zanetisaief@gmail.com') //link for email account for admin
+                ->subject('a User Tried To Contact You ! ')
+                ->htmlTemplate('contact_us/contactMail.html.twig')
+                ->context(["senderName"=>$name,"senderEmail"=>$email,"senderMessage"=>$message]) ;
 
-
+            try {
+                $mailer->send($mail);
+                $this->addFlash("success","Message Sent Successfully");
+            } catch (TransportExceptionInterface $e) {
+                $this->addFlash("error","Error while Sending message , Try again");
+            } finally {
+            return $this->render('contact_us/contact_us.twig',['form'=>$form->createView()])  ;
+            }
         }
-
-        return $this->render('contact_us/contact_us.twig',['form'=>$form->createView()]);
+        return $this->render('contact_us/contact_us.twig',['form'=>$form->createView()])  ;
     }
 }
